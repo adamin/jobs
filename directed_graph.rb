f60d6c19bd3f5bee9106ab526cf4a774aaf1ea72
adam <adam.wierzynkiewicz@gmail.com>
2017-02-18 18:18:17 +0000

    Implemented functionality of adding and finding vertices as well as adding edges to the graphs.

--- directed_graph.rb
+++ directed_graph.rb
@@ -8,12 +8,36 @@ require_relative 'vertex'
 #
 class DirectedGraph < Graph
 
-  # Public: Adds a new vertex to the graph. Overrides method from the parent.
-  #
-  # vertex  - instance of Vertex class to add
-  #
-  def add_vertex(vertex)
+  def initialize()
+    super()
+  end
 
+  # Public: Adds a new edge to the graph. Overrides method from the parent.
+  #
+  # start_vertex_name   - name of the starting vertex
+  # end_vertex_name     - name of the ending vertex
+  #
+  def add_edge(start_vertex_name, end_vertex_name)
+
+    # Check if graph is not empty
+    if (@vertices.length == 0)
+      raise ArgumentError, 'No edges can be added to an empty graph'
+    end
+
+    first_vertex_index = self.find_index_for_vertex(start_vertex_name)
+    second_vertex_index = self.find_index_for_vertex(end_vertex_name)
+
+    if (first_vertex_index == nil)
+      raise ArgumentError, 'Edge cannot be added. First vertex could not be found'
+    end
+
+    if (second_vertex_index == nil)
+      raise ArgumentError, 'Edge cannot be added. Second vertex could not be found'
+    end
+
+    @vertices[first_vertex_index].neighbours[second_vertex_index] = true
+
+    self
   end
 
 end
--- graph.rb
+++ graph.rb
@@ -20,8 +20,21 @@ class Graph
   # vertex  - instance of Vertex class to add
   #
   def add_vertex(vertex)
+    if (vertex.instance_of? Vertex)
+    @vertices << vertex
+    else
+      raise ArgumentError, 'Argument is not an instance of Vertex'
+    end
 
     self
+  end
+
+  # Public: Returns index in the vertices array for a vertex
+  #
+  # name  - name of the vertex to search for
+  #
+  def find_index_for_vertex(name)
+    result = @vertices.index { |v| v.name == name }
   end
 
   # Public: Finds a vertex by its name.
@@ -29,7 +42,13 @@ class Graph
   # name  - name of the vertex to find
   #
   def find_vertex(name)
+    result = nil
 
+    @vertices.each do |v|
+      result = v if v.name == name
+    end
+
+    result
   end
 
   # Public: Adds a new edge to the graph
@@ -38,6 +57,26 @@ class Graph
   # end_vertex_name     - name of the ending vertex
   #
   def add_edge(start_vertex_name, end_vertex_name)
+
+    # Check if graph is not empty
+    if (@vertices.length == 0)
+      raise ArgumentError, 'No edges can be added to an empty graph'
+    end
+
+    first_vertex_index = self.find_index_for_vertex(start_vertex_name)
+    second_vertex_index = self.find_index_for_vertex(end_vertex_name)
+
+    if (first_vertex_index == nil)
+      raise ArgumentError, 'Edge cannot be added. First vertex could not be found'
+    end
+
+    if (second_vertex_index == nil)
+      raise ArgumentError, 'Edge cannot be added. Second vertex could not be found'
+    end
+
+    @vertices[first_vertex_index].neighbours[second_vertex_index] = true
+    @vertices[second_vertex_index].neighbours[first_vertex_index] = true
+
     self
   end
 
