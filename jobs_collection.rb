56f8a4c4da6822e5512c6ea6c84992489b9c8e65
adam <adam.wierzynkiewicz@gmail.com>
2017-02-19 20:31:29 +0000

    Usage of previously implemented functionality within JobsCollection class to generate required sequence of jobs

--- jobs_collection.rb
+++ jobs_collection.rb
@@ -2,16 +2,42 @@
 #
 # Extends DirectedGraph class.
 class JobsCollection < DirectedGraph
+
   # Public: Initialises the collection with data about jobs and their dependencies
   #
   # data - contains jobs and their dependencies
   #
   def initialize(data)
+    super()
+    begin
+      self.allow_cycles = false
+      self.build(data)
+    rescue GraphError => e
+      case e.code
+      when GraphError::ERROR_DEPENDENCY_ON_ITSELF
+        raise ArgumentError,'Jobs must not depend on themselves'
+      else
+        raise ArgumentError, e.message
+      end
+    end
 
   end
 
-  # Generates desired sequence in which the jobs should be completed
+  # Public: Generates desired sequence in which the jobs should be completed
   def get_sequence()
+    result = []
+    begin
+      result = self.convert_indexes_to_names(self.perform_topological_sort)
+    rescue GraphError => e
+      case e.code
+      when GraphError::ERROR_UNEXPECTED_CYCLE
+        raise ArgumentError,'Jobs must not create circular dependencies'
+      else
+        raise ArgumentError, e.message
+      end
+    end
 
+    # Reverts the sequence
+    result.reverse
   end
 end
